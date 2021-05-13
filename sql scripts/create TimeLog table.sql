9d36db4a6dd27e677536de50a477015630f243a8
smelton22 <38869950+smelton22@users.noreply.github.com>
2021-05-12 21:22:37 -0400

    Update create TimeLog table.sql

--- sql scripts/create TimeLog table.sql
+++ sql scripts/create TimeLog table.sql
@@ -1,11 +1,13 @@
-create table tblTimeLog(
+create table tbltimelog(
 logId int auto_increment primary key,
 clockNumber integer not null,
 datefield date not null,
-dailyIn time,
-timeOut time,
-timeIn time,
-dailyOut time,
-notes varchar (50),
-approved boolean,
-fullcard boolean default false);
+dailyin time,
+timeout time,
+timein time,
+dailyout time,
+notes varchar(50),
+approved tinyint(1) default 0,
+fullcard tinyint(1) default 0,
+totalhours Decimal(5,2)
+);
